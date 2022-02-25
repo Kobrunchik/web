0c33ec356ef43e6460c5b82dd241b9fe60df8d91
Kobrunchik <kobrinf@mail.ru>
2022-02-25 16:36:27 +0100

    25/02/2022

---
+++ client/src/components/Article.vue
@@ -0,0 +1,31 @@
+<template>
+ <div class="card">
+   <h3>Title</h3>
+   <span>
+     <img src="https://picsum.photos/150/150">
+   </span>
+   <span>
+     Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus
+     saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.
+     Pellentesque ipsum. Aenean vel massa quis mauris vehicula lacinia. Etiam bibendum
+     elit eget erat. Curabitur ligula sapien, pulvinar a vestibulum quis, facilisis vel
+     sapien. Aliquam ante. Quisque porta. Curabitur bibendum justo non orci. Class
+     aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos.
+     Duis sapien nunc, commodo et, interdum suscipit, sollicitudin et, dolor. Duis aute
+     irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+     pariatur. Mauris tincidunt sem sed arcu. Duis bibendum, lectus ut viverra rhoncus,
+     dolor nunc faucibus libero, eget facilisis enim ipsum id lacus.
+   </span>
+
+ </div>
+</template>
+
+<script>
+export default {
+  name: "Article"
+}
+</script>
+
+<style scoped>
+
+</style>
